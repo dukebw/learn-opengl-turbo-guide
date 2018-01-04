406e4382d26c38056c87b2cbfc9357d59d199d70
Brendan Duke <dukebw@mcmaster.ca>
2018-01-04 08:58:21 -0500

    added up to glfwSetFramebufferSizeCallback, can't run on Sandy bridge...

--- hello_window.c
+++ hello_window.c
@@ -1,6 +1,14 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include <assert.h>
 #include <stdlib.h>
+
+#define UNUSED(x) x __attribute__ ((__unused__))
+
+void framebuffer_size_callback(GLFWwindow *UNUSED(window), int32_t width, int32_t height)
+{
+        glViewport(0, 0, width, height);
+}
 
 int main(void)
 {
@@ -8,6 +16,15 @@ int main(void)
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        GLFWwindow *window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+        assert(window != NULL);
+
+        assert(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) != 0);
+
+        glViewport(0, 0, 800, 600);
+
+        assert(glfwSetFramebufferSizeCallback(window, framebuffer_size_callback) != NULL);
 
         return EXIT_SUCCESS;
 }
