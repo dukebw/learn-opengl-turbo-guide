4f207e8feff3191d8f4797ff416998dfa9cd61a5
Brendan Duke <brendanw.duke@gmail.com>
2020-04-24 09:30:21 -0400

    Setup for Ex. 6.

--- anki/main.cpp
+++ anki/main.cpp
@@ -1,35 +1,14 @@
-#include "learngl.hpp"
-#include <cstdlib>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-        GLFWwindow* window = init_gl(800, 600);
-        if (window == NULL)
-                return -1;
-
-        uint32_t vertex = create_shader("./shader.vert", GL_VERTEX_SHADER);
-        uint32_t fragment = create_shader("./shader.frag", GL_FRAGMENT_SHADER);
-        uint32_t shader_prog = create_shader_program(vertex, fragment);
-        float vertices[] = {
-                -0.5f, -0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f,
-                0.5f,  0.5f, 0.0f,
-        };
-        uint32_t VAO = init_VAO(vertices, sizeof(vertices));
-
-        init_vert_attr(0, 3, 3, 0);
-
-        glUseProgram(shader_prog);
-        while (!glfwWindowShouldClose(window)) {
-                glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-                glClear(GL_COLOR_BUFFER_BIT);
-
-                glBindVertexArray(VAO);
-                glDrawArrays(GL_TRIANGLES, 0, 3);
-
-                glfwSwapBuffers(window);
-                glfwPollEvents();
-        }
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         glfwTerminate();
 
